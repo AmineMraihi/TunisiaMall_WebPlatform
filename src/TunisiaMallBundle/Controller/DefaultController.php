06fa9615de0e9023ee2ff86bd401bd4cb6ef8c9f
AmineMraihi <amine.mraihi@esprit.tn>
2017-11-16 20:10:34 +0100

    added admin template + signup (needs some work) + crud events (simple crud )

--- src/TunisiaMallBundle/Controller/DefaultController.php
+++ src/TunisiaMallBundle/Controller/DefaultController.php
@@ -3,6 +3,8 @@
 namespace TunisiaMallBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use TunisiaMallBundle\Entity\User;
 
 class DefaultController extends Controller
 {
@@ -13,6 +15,46 @@ class DefaultController extends Controller
     public function ContactAction()
     {
         return $this->render('TunisiaMallBundle:Default:Contact.html.twig');
+    }
+
+    public function adminevenementAction()
+    {
+        return $this->render('TunisiaMallBundle::evenement.html.twig');
+    }
+
+    public function adminindexAction()
+    {
+        return $this->render('TunisiaMallBundle::adminindex.html.twig');
+    }
+
+    public function signupAction(Request $request)
+    {
+        $user = new User();
+
+        if ($request->isMethod('POST')) {
+            $user->setEmail($request->get("email"));
+            $user->setUsername($request->get("username"));
+            $user->setPassword($request->get("password"));
+            $user->setNom($request->get("nom"));
+            $user->setPrenom($request->get("prenom"));
+            $user->setSexe($request->get("sexe"));
+            $user->setNumeroTelephone($request->get("numero_telephone "));
+            $user->setAdresse($request->get("adresse"));
+            $user->setSalaire($request->get("salaire"));
+            $user->setPath($request->get("path"));
+            $user->setIdBoutique($request->get("id_boutique"));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+        }
+        return $this->render("TunisiaMallBundle::signup.html.twig",
+            array()
+        );
+    }
+
+    public function aboutAction()
+    {
+        return $this->render('TunisiaMallBundle:Default:about.html.twig');
     }
 
 }
